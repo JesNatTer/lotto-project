c4e61e9c2a6c64e940e89d4bbf916167ecb37560
JesNatTer <jessenterblanche@gmail.com>
2021-06-21 15:53:16 +0200

    update

--- functionstotest.py
+++ functionstotest.py
@@ -1,16 +1,9 @@
-# class TestFunctions:
-#     def lotto(self, entry1, randomset1):
-#         count = 0
-#         entry = entry1
-#         randomset = randomset1
-#         for x in range(3):
-#             if entry[x] == randomset[x]:
-#                 count += 1
-#         return count
-
-
-text = open('playerlog.txt', "+r")
-list2 = []
-for line in text:
-    list2.append(text.readline())
-print(list2[])
+class TestFunctions:
+    def lotto(self, entry1, randomset1):
+        count = 0
+        entry = entry1
+        randomset = randomset1
+        for x in range(3):
+            if entry[x] == randomset[x]:
+                count += 1
+        return count
